10303a041e51c72ebe43fae48fe6c79e87e1fde5
MichéLozano <elmiche@latinooutdoors.org>
2020-01-02 22:17:08 -0800

    todo list

--- DJANGO/my_todo_list/todo_app/views.py
+++ DJANGO/my_todo_list/todo_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.http import HttpResponse
+
+from .models import Todo
+
+def todo_list(request):
+    return HttpResponse('hello from the todo_list view')
+
+def details(request, id):
+    return HttpResponse('hello from the details view')
+
+def add_todo(request):
+    return HttpResponse('hello from the add_todo view')
+
+def remove_todo(request, id):
+    return HttpResponse('hello from the remove view')
+
+def update_todo(request, id):
+    return HttpResponse('hello from the update_todo view')
+
+def update(request, id):
+    return HttpResponse('hello from the update view')
+
+
+
