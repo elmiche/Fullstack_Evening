651ae6d128ae3b55548f4070bd06cac923698176
MichéLozano <elmiche@latinooutdoors.org>
2020-01-03 20:37:44 -0800

    Revert "views update"
    Ths reverts commit c18b5dad6c94827c19bc3d79638b1c72b920a983.
    
    q
    
    q!
    
    quit!

--- DJANGO/my_todo_list/todo_app/views.py
+++ DJANGO/my_todo_list/todo_app/views.py
@@ -4,9 +4,6 @@ from django.http import HttpResponse
 
 from .models import Todo
 
-def home(request)
-    return HttpResponse('<h1>Blog Home</h1>')
-
 def todo_list(request):
     return HttpResponse('hello from the todo_list view')
 
