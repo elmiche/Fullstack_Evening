3912c4daabaad697a5859f7af6acf5e4d8111655
MichéLozano <elmiche@latinooutdoors.org>
2020-01-12 22:47:59 -0800

    url views

--- DJANGO/url_shorter/shorter_url_app/views.py
+++ DJANGO/url_shorter/shorter_url_app/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 from .models import Shorter_url
 from django.http import HttpResponse
+from django.utils.crypto import get_random_string
+
+
+import random
 
 # Create your views here.
+
 
 def home(request):
     if request.method == 'POST':
         url = request.POST['url']
         Shorter_url.objects.create(url = url)
+        return render(request, 'shorter_url_app/my_urls.html', )
     else:
-        return HttpResponse('shorter_url_app/app_home.html')
-        # return render(request, 'shorter_url_app/app_home.html')
-    # return HttpResponse('hello')
+        # return HttpResponse('shorter_url_app/app_home.html')
+        return render(request, 'shorter_url_app/app_home.html', )
 
+
+def url_strgen(url):
+    short_url_id = get_random_string(legnth=20) #renames url, and resizing 
+    resized_url = Shorter_url.objects.create(shorty = short_url_id) #saves to the database ??
+    resized_url.save() #calls the variable assigned to the DB
+    print(short_url_id) 
+
+
+    
+# TODO: supply both shorty and url from the Model in the create
+#Pass both the short code and the long url in the create()
+#ERROR: 'table shorter_url_app_shorter_url has no column named shorty'
+#how does info in the models get to the DB... columns???
