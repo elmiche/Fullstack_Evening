a8f74352b1d21612b1ac95ad5258283a915ba241
MichéLozano <elmiche@latinooutdoors.org>
2020-01-14 21:06:26 -0800

    Home page connected

--- DJANGO/basic_CRUD/crud_app/views.py
+++ DJANGO/basic_CRUD/crud_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        entry = request.POST['form-entry']
+        return redirect('app-home') #returns to the home page with the form
+    else:
+        return render(request,'pages/home.html')
+        #render is for templates
