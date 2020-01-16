80c372950b7874eb1ba39346793728cd6d5f43b6
MichéLozano <elmiche@latinooutdoors.org>
2020-01-15 21:11:19 -0800

    media commits

--- DJANGO/Media-master/media_app/views.py
+++ DJANGO/Media-master/media_app/views.py
@@ -11,10 +11,10 @@ def index(request):
 
 def image_upload(request):
     if request.method == 'POST':
-        image_file = request.FILES.get('image_file')
-        upload = MyModel(my_image = image_file)
+        image_file = request.FILES.get('image_file') #gets file from the form under the same name 'image_file'
+        upload = MyModel(my_image = image_file) #save the file to the variable 
         upload.save()
-        image_url = upload.my_image.url
+        image_url = upload.my_image.url #saving the file you got to the model 
         return redirect('index')
         
     else:
