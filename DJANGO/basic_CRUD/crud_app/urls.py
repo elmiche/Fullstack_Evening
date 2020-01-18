c4b904630055a89da015634d532a7fdb4057252b
MichéLozano <elmiche@latinooutdoors.org>
2020-01-17 21:29:55 -0800

    urls update

--- DJANGO/basic_CRUD/crud_app/urls.py
+++ DJANGO/basic_CRUD/crud_app/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.create, name = 'app-home'),
     path('read-list/', views.read, name = 'read-list'), #creating the path to the read view, return the name 'read-list' in the view function!
+    path('remove-item/<int:id>', views.remove, name = 'remove-item'),
+    path('update/<int:id>', views.update, name ='update'),
     path('admin/', admin.site.urls),
 ]
