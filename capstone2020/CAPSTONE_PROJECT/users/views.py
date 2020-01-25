a2045607ba27b4feac401d3d48d4b439ad06635c
MichéLozano <elmiche@latinooutdoors.org>
2020-01-24 16:35:59 -0800

    registration crsipy form

--- capstone2020/CAPSTONE_PROJECT/users/views.py
+++ capstone2020/CAPSTONE_PROJECT/users/views.py
@@ -12,7 +12,11 @@ def register(request):
             #flash message - one-time alerts.
             messages.success(request, f'Account created for {username}!')
             return redirect('app-home')
+        else: 
+            messages.warning(request, f'Failed. Please see below and try again.')
+            return render(request, 'users/register.html', {'form': form})
     else:        
         form = UserRegisterForm()
         return render(request, 'users/register.html', {'form': form})
 
+#TODO add login view + template + route
