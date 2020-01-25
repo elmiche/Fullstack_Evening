83b82e70ee26d0a82cf273bc4f22576699addc8b
MichéLozano <elmiche@latinooutdoors.org>
2020-01-24 20:12:47 -0800

    user profile added

--- capstone2020/CAPSTONE_PROJECT/capstone_project2020/settings.py
+++ capstone2020/CAPSTONE_PROJECT/capstone_project2020/settings.py
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'app-home'
+
+LOGIN_URL = 'login'
--- capstone2020/CAPSTONE_PROJECT/users/views.py
+++ capstone2020/CAPSTONE_PROJECT/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -19,4 +20,6 @@ def register(request):
         form = UserRegisterForm()
         return render(request, 'users/register.html', {'form': form})
 
-#TODO add login view + template + route
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
