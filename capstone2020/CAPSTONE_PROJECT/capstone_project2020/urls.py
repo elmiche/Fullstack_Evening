94e4cc1751dbe096aa0f54ed2b8a318d81bc6434
MichéLozano <elmiche@latinooutdoors.org>
2020-01-30 18:47:18 -0800

    urls merge

--- capstone2020/CAPSTONE_PROJECT/capstone_project2020/urls.py
+++ capstone2020/CAPSTONE_PROJECT/capstone_project2020/urls.py
@@ -28,11 +28,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
 ]
-<<<<<<< HEAD
-  
-=======
  
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> modelMarkers
