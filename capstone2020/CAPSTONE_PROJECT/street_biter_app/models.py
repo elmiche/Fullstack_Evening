0a16cc41f12ccd48b7c3e7cd577bb9a5e945b5f7
MichéLozano <elmiche@latinooutdoors.org>
2020-01-31 21:06:02 -0800

    models defaults

--- capstone2020/CAPSTONE_PROJECT/street_biter_app/models.py
+++ capstone2020/CAPSTONE_PROJECT/street_biter_app/models.py
@@ -6,10 +6,10 @@ from django.contrib.auth.models import User
 class Street_biter(models.Model):
     date_posted =models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    latitude = models.DecimalField(max_digits=7, decimal_places=7)
-    longitude = models.DecimalField(max_digits=7, decimal_places=7) 
-    details = models.CharField(max_length=900)
-    species = models.CharField(max_length=20)
+    latitude = models.DecimalField(max_digits=7, decimal_places=7, default=0)
+    longitude = models.DecimalField(max_digits=7, decimal_places=7, default=0) 
+    details = models.CharField(max_length=900, default='add details')
+    species = models.CharField(max_length=20, default='add details')
     # user = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
 
 # class Markers(models.Model):
