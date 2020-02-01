55a041f1d87fa67926570c9cb2a111d38f6e7418
MichéLozano <elmiche@latinooutdoors.org>
2020-01-31 21:07:53 -0800

    model update

--- capstone2020/CAPSTONE_PROJECT/street_biter_app/models.py
+++ capstone2020/CAPSTONE_PROJECT/street_biter_app/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # from django.contrib.auth.models import get_user_model
 
 class Street_biter(models.Model):
-    date_posted =models.DateTimeField(default=timezone.now)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted =models.DateTimeField(default=timezone.now, default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     latitude = models.DecimalField(max_digits=7, decimal_places=7, default=0)
     longitude = models.DecimalField(max_digits=7, decimal_places=7, default=0) 
     details = models.CharField(max_length=900, default='add details')
